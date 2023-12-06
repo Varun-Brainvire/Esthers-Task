cb8fe88a6e7409d280d178222344267385e8d2da
Ajay Choudhary <ajay.choudhary@brainvire.com>
2023-12-06 19:33:21 +0530

    First Commit

---
+++ src/pages/howitworks/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { MainContainer } from "./howitStyle";
+import { NavbarContainer } from "@/components/Navbar/NavbarStyle";
+
+const index = () => {
+  return (
+    <>
+      <MainContainer>
+        <NavbarContainer></NavbarContainer>
+      </MainContainer>
+    </>
+  );
+};
+
+export default index;
