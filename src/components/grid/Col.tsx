61d287c1fccc3227d39a778b2728ea0c275567f7
Riddhi Parmar <riddhi.parmar@brainvire.com>
2023-12-14 13:15:10 +0530

    changed col file

--- src/components/grid/Col.tsx
+++ src/components/grid/Col.tsx
@@ -1,12 +1,15 @@
-import styled from "styled-components"
-import { media, Col as sbgCol } from "styled-bootstrap-grid"
+import styled from "styled-components";
+import { media, Col as sbgCol } from "styled-bootstrap-grid";
 
-export const Col = styled(sbgCol)`
-  padding: 0;
-  margin: 0;
-  /* padding-left: 10px;
-  padding-right: 10px; */
-  /* 
+interface ColProps {
+  padding?: boolean;
+  paddingInBanner?: boolean;
+}
+
+export const Col = styled(sbgCol)<ColProps>`
+  padding-left: 5px;
+  padding-right: 5px;
+
   ${media.lg`
   padding-left: 10px;
   padding-right: 10px;
@@ -23,6 +26,7 @@ export const Col = styled(sbgCol)`
   `}
 
   ${media.md`
+
   @media (min-width: 768px) {
     padding-left: ${({ paddingInBanner }) => (paddingInBanner ? "10px" : "0")};
     padding-right: ${({ paddingInBanner }) => (paddingInBanner ? "10px" : "0")};
@@ -30,7 +34,23 @@ export const Col = styled(sbgCol)`
   `}
 
    ${media.sm`
-  padding-left: 10px !important;
-  padding-right: 10px !important;
-  `} */
-`
+   padding-left: 5px;
+   padding-right: 5px;
+  `}
+
+  
+  @media (min-width: 768px) {
+    padding-left: ${({ isMobile }) => (isMobile ? "0" : "5px")};
+    padding-right: ${({ isMobile }) => (isMobile ? "0" : "5px")};
+  }
+
+  @media (max-width: 375px) {
+    flex: ${({ padding }) => (padding ? "0 0 auto" : "")};
+    min-width: ${({ padding }) => (padding ? "300px" : "")};
+  }
+
+  @media (max-width: 375px) {
+    padding-left: ${({ padding }) => (padding ? "5px" : "0")};
+    padding-right: ${({ padding }) => (padding ? "5px" : "0")};
+  }
+`;
