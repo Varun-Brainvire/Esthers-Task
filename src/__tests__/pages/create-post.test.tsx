76239f1eab9d33bc0c13070fd0a7ccc0d109d800
RRP820823 <rushikesh.patil@brainvire.com>
2023-12-18 11:06:53 +0530

    Create a test file and added few tests , need to add snapshot testing as well

---
+++ src/__tests__/pages/create-post.test.tsx
@@ -0,0 +1,61 @@
+import "@testing-library/jest-dom"
+import { render, screen } from "@testing-library/react"
+import FlotingMenu from "@/create-post-component.tsx/FlotingMenu"
+import Page from "../../pages/create-post"
+import StepButtons from "@/components/createPost/stepButtons/stepButtons"
+import BottomButtons, {
+  Button,
+} from "@/components/createPost/bottomButtons/bottomButtons"
+import { Styled_Button } from "@/components/createPost/bottomButtons/bottomButtons.styles"
+import { Input } from "@/create-post-component.tsx/FlotingMenu.styles"
+it("renders a page", () => {
+  render(<Page />)
+  const heading = screen.getByText("Create Image Post")
+  screen.debug()
+  expect(heading).toBeInTheDocument()
+})
+
+it("renders a button", () => {
+  render(<Page />)
+  const button = screen.getByText("Apply to be Creator")
+
+  expect(button).toBeInTheDocument() // Asserts the button is present
+})
+
+it("renders a StepButtons ", () => {
+  render(<StepButtons />)
+  const button1 = screen.queryAllByText("Add caption & hashtags")
+})
+
+it("renders a StepButtons ", () => {
+  const label = "Click me"
+  const { getByTestId } = render(<Button label={label} />)
+
+  const button = getByTestId(label)
+
+  expect(button).toBeInTheDocument()
+})
+
+// Input
+
+it("renders a click to upload or drag and drop input label  ", () => {
+  render(<Page />)
+  let label = screen.getByText(/click to upload or drag and drop/i)
+  expect(label).toBeInTheDocument()
+})
+
+it("renders a choose from instagram label   ", () => {
+  render(<Page />)
+  let label = screen.getByText(/choose from instagram/i)
+  expect(label).toBeInTheDocument()
+})
+
+// getByText(/click to upload or drag and drop/i)
+
+it("renders A Save as Draft button   ", () => {
+  render(<Page />)
+  let label = container.querySelector(
+    "#sandbox > div > div > div > div > div:nth-child(2) > div > div:nth-child(2) > div:nth-child(2) > div:nth-child(1)"
+  )
+  expect(label).toBeInTheDocument()
+})
